89c7d28768ed47cfbd598db1959883f141f6a410
Owais <soac@outlook.com>
2015-11-30 23:05:05 +0500

    Fix warning in addons.php

--- admin/addons/addons.php
+++ admin/addons/addons.php
@@ -5,7 +5,6 @@ if(get_option('hatchbuck_addons_form-widget')) {
 }
 
 if($data = get_option('hatchbuck_addons_scroll-box')) {
-    if (wp_is_mobile() && $data['hb_show_mobile'] == 0) return;
-    
+    if (wp_is_mobile() && isset($data['hb_show_mobile']) && $data['hb_show_mobile'] == 0) return;
     require( dirname( __FILE__ ) . '/scroll-box/scroll-box.php' );
 }
