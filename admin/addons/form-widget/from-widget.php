fa5698063521e28cfe34adf075cf8fd66c72aacf
cargie <cargiejayectin@gmail.com>
2017-03-20 05:58:59 +0800

    use parent::__construct instead of parent::WP_Widget

--- admin/addons/form-widget/from-widget.php
+++ admin/addons/form-widget/from-widget.php
@@ -6,89 +6,96 @@
 
 ////*****************************Sidebar Widget**********************************////
 
-class HatchbuckWidget extends WP_Widget {
- 
- 
+class HatchbuckWidget extends WP_Widget
+{
+
     /** constructor -- name this the same as the class above */
-    function HatchbuckWidget() {
-        //parent::WP_Widget(false, $name = 'Hatchbuck Forms');	
-        $widget_ops = array('classname' => 'HatchbuckWidget', 'description' => 'Adds Hatchbuck forms to your sidebar' );
-        $this->WP_Widget('HatchbuckWidget', 'Hatchbuck Forms', $widget_ops);
+    public function __construct()
+    {
+        //parent::WP_Widget(false, $name = 'Hatchbuck Forms');
+        $widget_ops = array('classname' => 'HatchbuckWidget', 'description' => 'Adds Hatchbuck forms to your sidebar');
+        parent::__construct('HatchbuckWidget', 'Hatchbuck Forms', $widget_ops);
 
     }
- 
+
     /** @see WP_Widget::widget -- do not rename this */
-    function widget($args, $instance) {	
-        extract( $args );
+    public function widget($args, $instance)
+    {
+        extract($args);
         global $wpdb;
-        $title 		= apply_filters('widget_title', $instance['title']);
-       	$hatchbuck_id = $instance['message'];
-  
-        $entries = $wpdb->get_results($wpdb->prepare( "SELECT content FROM ".$wpdb->prefix.HATCHBUCK_TABLE."  WHERE id=%d",$hatchbuck_id ));
-        
+        $title        = apply_filters('widget_title', $instance['title']);
+        $hatchbuck_id = $instance['message'];
+
+        $entries = $wpdb->get_results($wpdb->prepare("SELECT content FROM " . $wpdb->prefix . HATCHBUCK_TABLE . "  WHERE id=%d", $hatchbuck_id));
+
         $entry = $entries[0];
 
         echo $before_widget;
-        if ( $title )
-        echo $before_title . $title . $after_title;
-		echo do_shortcode($entry->content);
-							
+        if ($title) {
+            echo $before_title . $title . $after_title;
+        }
+
+        echo do_shortcode($entry->content);
+
         echo $after_widget;
-        
+
     }
- 
+
     /** @see WP_Widget::update -- do not rename this */
-    function update($new_instance, $old_instance) {		
-		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['message'] = strip_tags($new_instance['message']);
+    public function update($new_instance, $old_instance)
+    {
+        $instance            = $old_instance;
+        $instance['title']   = strip_tags($new_instance['title']);
+        $instance['message'] = strip_tags($new_instance['message']);
         return $instance;
     }
- 
+
     /** @see WP_Widget::form -- do not rename this */
-    function form($instance) {	
-    	global $wpdb;
-    	$entries = $wpdb->get_results($wpdb->prepare( "SELECT * FROM ".$wpdb->prefix.HATCHBUCK_TABLE." WHERE status=%d  ORDER BY id DESC",1 ));
-    	
-    	
-    	if(isset($instance['title'])){
-    		$title	= esc_attr($instance['title']);
-    	}else{
-    		$title = '';
-    	}
-    	
-    	if(isset($instance['message'])){
-    		$message	= esc_attr($instance['message']);
-    	}else{
-    		$message = '';
-    	}
-    	
+    public function form($instance)
+    {
+        global $wpdb;
+        $entries = $wpdb->get_results($wpdb->prepare("SELECT * FROM " . $wpdb->prefix . HATCHBUCK_TABLE . " WHERE status=%d  ORDER BY id DESC", 1));
+
+        if (isset($instance['title'])) {
+            $title = esc_attr($instance['title']);
+        } else {
+            $title = '';
+        }
+
+        if (isset($instance['message'])) {
+            $message = esc_attr($instance['message']);
+        } else {
+            $message = '';
+        }
+
         ?>
          <p>
-          <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+          <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:');?></label>
           <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
         </p>
 		<p>
-          <label for="<?php echo $this->get_field_id('message'); ?>" style="display:block"><?php _e('Select your form :'); ?></label> 
-          
+          <label for="<?php echo $this->get_field_id('message'); ?>" style="display:block"><?php _e('Select your form :');?></label>
+
           <select name="<?php echo $this->get_field_name('message'); ?>" style="width:100%">
-          <?php 
-					if( count($entries)>0 ) {
-						$count=1;
-						$class = '';
-						foreach( $entries as $entry ) {
-					?>
-					<option value="<?php echo $entry->id;?>" <?php if($message==$entry->id)echo "selected"; ?>><?php echo $entry->title;?></option>
-					<?php 		
-						}
-					}
-					?>
+          <?php
+if (count($entries) > 0) {
+            $count = 1;
+            $class = '';
+            foreach ($entries as $entry) {
+                ?>
+					<option value="<?php echo $entry->id; ?>" <?php if ($message == $entry->id) {
+                    echo "selected";
+                }
+                ?>><?php echo $entry->title; ?></option>
+					<?php
+}
+        }
+        ?>
           </select>
         </p>
-        <?php 
-    }
- 
- 
+        <?php
+}
+
 } // end class HatchbuckWidget
 add_action('widgets_init', create_function('', 'return register_widget("HatchbuckWidget");'));
 ?>
