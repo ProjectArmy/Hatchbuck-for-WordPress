5d262b23ede9c9758f9ee36133f442bfbf2b6d96
Viktor Nagornyy <vnagornyy@viktorix.com>
2018-12-16 01:09:37 -0500

    Updated text
    Updated sidebar text.

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -2,19 +2,18 @@
    <div class="postbox">
     <h3 class="hndle"><span>Need help with WordPress?</span></h3>
     <div class="inside">
-    <p>We help small service businesses turn their website into an asset, not a liability.</p>
+    <p>We help small businesses turn their website into an asset, not a liability.</p>
       <ul class="hb-list">
-        <li>Performance, search engine & conversion optimizations</li>
+        <li>Custom styles & multi-step Hatchbuck forms</li>
+        <li>Performance optimization & troubleshooting</li>
         <li>Website security & malware cleanup</li>
         <li>Proactive upgrades & maintenance</li>
-        <li>Uptime monitoring & response</li>
-        <li>Dedicated marketing strategist</li>
-        <li>Monthly marketing report</li>
+        <li>Migrations between hosts</li>
+        <li>Website emergencies</li>
         <li>and much more!</li>
     </ul>
-    <p>Take care of your clients, <br />we'll take care of you.</p>
-    <a href="https://www.projectarmy.net/solutions/wordpress-care/?utm_source=<?php echo $_SERVER['HTTP_HOST']; ?>&utm_medium=Orange%20Button&utm_campaign=Hatchbuck%20Plugin&utm_content=WordPress%20Care" target="_blank" class="hb-btn hb-btn-block" />Learn About WordPress Care &raquo;</a>
-    <small>Plans start at $59/mo! On-demand services available.</small>
+    <p>Take care of your business, <br />we'll take care of you.</p>
+    <a href="https://www.projectarmy.net/?utm_source=<?php echo $_SERVER['HTTP_HOST']; ?>&utm_medium=Orange%20Button&utm_campaign=Hatchbuck%20Plugin&utm_content=WordPress%20Services" target="_blank" class="hb-btn hb-btn-block" />Get WordPress Help &raquo;</a>
     </div><!-- inside -->
   </div><!-- postbox -->
 </div><!-- postbox-container-1 -->
